b65d46fe0286e30bd6ead1a3b2969123d04208b3
Tobias Schroefel <tschroefel@gmail.com>
2018-06-23 14:43:26 +0200

    * ambience light now has to be "dark" enough for at least 5 seconds before the animations will start * deflickering of LCD in certain cases * boot delay upped to 5 seconds (ensures etherned shield is fully powered up before proceeding)

--- MoodHorst.ino
+++ MoodHorst.ino
@@ -9,66 +9,75 @@
 // thankfully based on the code examples of:
 // -Mark Kriegsman, December 2014
 
-FASTLED_USING_NAMESPACE                                       // Initialize WS2812B matrix and required config
-#define DATA_PIN    5
-#define AMBIENCE_LIGHT_PIN  0                                 // For input from photocell (or potentiometer) to dim the leds by external input
-int reading = 0;
+FASTLED_USING_NAMESPACE                                         // Initialize WS2812B matrix and required config
+#define DATA_PIN    5                                           // Pin used for communicating with the WS812B LEDs
+#define AMBIENCE_LIGHT_PIN  0                                   // For input from photocell (or potentiometer) to dim the leds by external input
+int reading = 0;                                                
 #define LED_TYPE    WS2811
 #define COLOR_ORDER GRB
 #define NUM_LEDS    256
 CRGB leds[NUM_LEDS];
 #define BASE_BRIGHTNESS     160
-#define FRAMES_PER_SECOND  120
+#define FRAMES_PER_SECOND  240
+int startFlag = 0;
+unsigned long startTime;
+unsigned long previousTime;
 
 bool on;
 int buzzer = 4;
 
-CRGBPalette16 currentPalette = PartyColors_p;                 // Palette definitions
+CRGBPalette16 currentPalette = PartyColors_p;                   // Palette definitions
 CRGBPalette16 targetPalette = PartyColors_p;
-TBlendType    currentBlending = LINEARBLEND;                  // NOBLEND or LINEARBLEND
+TBlendType    currentBlending = LINEARBLEND;                    // NOBLEND or LINEARBLEND
 
-LiquidCrystal lcd(7, 8, 0, 1, 2, 3);                          // Initialize LCD
+LiquidCrystal lcd(7, 8, 0, 1, 2, 3);                            // Initialize LCD
 
 // Ethernet & doorstatus
-byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };          // physical mac address
-char serverName[] = "10.234.2.103";                           
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };            // physical mac address
+char serverName[] = "10.234.2.103";
 EthernetClient client;
 String doorstatus;
 String readString;
-int x = 0;                                                    // for counting line feeds
-char lf = 10;                                                 // line feed character
+int x = 0;                                                      // for counting line feeds
+char lf = 10;                                                   // line feed character
 int i = 0;
 EthernetUDP Udp;
 NTPClient timeClient(Udp, 7200);
 
-String getDoorstatus()                                        // client function to send/receive GET request data.
+String getDoorstatus()                                          // client function to send/receive GET request data.
 {
   x = 0;
   i++;
   readString = ("");
-  if (client.connect(serverName, 8090)) {                     // starts client connection, checks for connection
-    client.println("GET /doorstatus/status.txt HTTP/1.1");    // download text
+  if (client.connect(serverName, 8090)) {                       // starts client connection, checks for connection
+    client.println("GET /doorstatus/status.txt HTTP/1.1");      // download text
     client.println("Host: 10.234.2.103");
-    client.println("Connection: close");                      // close 1.1 persistent connection
-    client.println();                                         // end of get request
+    client.println("Connection: close");                        // close 1.1 persistent connection
+    client.println();                                           // end of get request
   }
 
-  while (client.connected() && !client.available()) delay(1); // waits for data
-  while (client.connected() || client.available()) {          // connected or data available
-    char c = client.read();                                   // gets byte from ethernet buffer
-    if (c == lf) x = (x + 1);                                 // counting line feeds
-    if (x == 10) readString += c;                             // building readString
+  while (client.connected() && !client.available()) delay(1);   // waits for data
+  while (client.connected() || client.available()) {            // connected or data available
+    char c = client.read();                                     // gets byte from ethernet buffer
+    if (c == lf) x = (x + 1);                                   // counting line feeds
+    if (x == 10) readString += c;                               // building readString
   }
   lcd.setCursor(0, 0);
   readString.trim();
   doorstatus = readString;
-  client.stop();                                              // stop client
+  client.stop();                                                // stop client
   return readString;
 }
 
 void setup() {
-  lcd.begin(16, 2);                                           // display boot screen with three second delay for power up recovery
+  lcd.begin(16, 2);                                             // display boot screen with three second delay for power up recovery
   lcd.setCursor(0, 0);
+  lcd.print("Booting up ... 5");
+  delay(1000);
+  lcd.clear();
+  lcd.print("Booting up ... 4");
+  delay(1000);
+  lcd.clear();
   lcd.print("Booting up ... 3");
   delay(1000);
   lcd.clear();
@@ -101,7 +110,7 @@ void setup() {
 }
 
 typedef void (*SimplePatternList[])();                          // List of patterns to cycle through.  Each is defined as a separate function below.
-SimplePatternList Patterns = { confetti, sinelon };
+SimplePatternList Patterns = { sinelon, confetti };
 
 uint8_t CurrentPattern = 0;                                     // Index number of which pattern is current
 uint8_t Hue = 0;                                                // rotating "base color" may be used by patterns
@@ -135,7 +144,7 @@ void loop()
   int min_hue = 0;
   int max_hue = 255;
 
-  if (doorstatus == "closed") {                                   // check doorstatus and set appropriate hue limits
+  if (doorstatus == "closed") {                                 // check doorstatus and set appropriate hue limits
     min_hue = 0;
     max_hue = 63;
   }
@@ -171,35 +180,52 @@ void loop()
       on = true;
       delay(50);
     }
-  } else {                                                    // clear LEDs after alert animation
+  } else {                                                      // clear LEDs after alert animation
     if (dateTime == "22:01:00") {
       FastLED.clear(true);
     }
     if (reading < 200 ) {
-      FastLED.setBrightness(cur_brightness);
-      Patterns[CurrentPattern]();                             // Call the current pattern function once, updating the 'leds' array
-      FastLED.show();                                         // send the 'leds' array out to the actual LED strip
-      FastLED.delay(1000 / FRAMES_PER_SECOND);                // insert a delay to keep the framerate modest
-
-      EVERY_N_MILLISECONDS( 80 ) {                            // do some periodic updates
-        Hue++;                                                // cycle the "base color" through the rainbow
+      if ((reading < 200) && (startFlag == 0) ) {               // ensure ambience light reading is < 200 for at least 5 seconds
+        startFlag = 1;
+        startTime = millis();
+        previousTime = startTime;
       }
-      EVERY_N_SECONDS( 60 ) {
-        nextPattern();                                        // change pattern every 60 seconds (if there is more than one)
+      unsigned long currTime = millis();
+      previousTime = previousTime++;
+      if ((currTime - startTime ) <= 5000) {                    // still waiting it out
+        if (reading > 200) {
+          startFlag = 0;
+        }
+        delay(100);                                         
+      }
+      if ((currTime - startTime) > 5000) {                      // ambience light has been "dark" enough for long enough
+        FastLED.setBrightness(cur_brightness);
+        Patterns[CurrentPattern]();                             // Call the current pattern function once, updating the 'leds' array
+        FastLED.show();                                         // send the 'leds' array out to the actual LED strip
+        FastLED.delay(1000 / FRAMES_PER_SECOND);                // insert a delay to keep the framerate modest
+
+        EVERY_N_MILLISECONDS( 80 ) {                            // do some periodic updates
+          Hue++;                                                // cycle the "base color" through the rainbow
+        }
+        EVERY_N_SECONDS( 60 ) {
+          nextPattern();                                        // change pattern every 60 seconds (if there is more than one)
+        }
       }
     } else {
-      FastLED.clear(true);
+      delay(300);                                               // add a delay to deflicker the LCD when not running time consuming
+      FastLED.clear(true);                                      // animations, also keep the LEDs cleared.
+      startFlag = 0;
     }
   }
 }
 
 #define ARRAY_SIZE(A) (sizeof(A) / sizeof((A)[0]))
 
-void nextPattern()                                            // add one to the current pattern number, and wrap around at the end
+void nextPattern()                                              // add one to the current pattern number, and wrap around at the end
 {
   CurrentPattern = (CurrentPattern + 1) % ARRAY_SIZE( Patterns);
 
-  getDoorstatus();                                            // fetch current doorstatus
+  getDoorstatus();                                              // fetch current doorstatus
 }
 
 void confetti()
@@ -213,7 +239,7 @@ void confetti()
 
 void sinelon()
 {
-  fadeToBlackBy( leds, NUM_LEDS, 20);                         // a colored dot sweeping back and forth, with fading trails
+  fadeToBlackBy( leds, NUM_LEDS, 20);                           // a colored dot sweeping back and forth, with fading trails
   int pos = beatsin16( 13, 0, NUM_LEDS - 1 );
   leds[pos] += CHSV( Hue, 255, 192);
 }
